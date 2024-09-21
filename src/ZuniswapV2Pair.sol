3d745ef7263377090fcf4c28b246568570913b02
day0xy <jasonfan1337@gmail.com>
2024-09-21 12:22:14 +0800

    update ZuniswapV2Pair.sol

--- src/ZuniswapV2Pair.sol
+++ src/ZuniswapV2Pair.sol
@@ -6,13 +6,127 @@ import "./libraries/Math.sol";
 import "./libraries/UQ112x112.sol";
 import "./interfaces/IZuniswapV2Callee.sol";
 
-contract ZuniswapV2Pair is ERC20, Math {
-    uint256 private reserve0;
-    uint256 private reserve1;
+interface IERC20 {
+    function balance0f(address) external returns (uint256);
+    function transfer(address to, uint256 amount) external;
+}
 
+//error和require和assert对比，消耗gas最少
+error AlreadyInitialized();
+error BalanceOverflow();
+error InsufficientInputAmount();
+error InsufficientLiquidity();
+error InsufficientLiquidityBurned();
+error InsufficientLiquidityMinted();
+error InsufficientOutputAmount();
+error InvalidK();
+error TransferFailed();
+
+contract ZuniswapV2Pair is ERC20, Math {
+    using UQ112x112 for uint224;
+    uint256 public constant MINIMUM_LIQUIDITY = 10 ** 3;
+
+    address public token0;
+    address public token1;
+
+    uint112 private reserve0;
+    uint112 private reserve1;
+
+    uint32 private blockTimestampLast;
+    uint256 public price0CumulativeLast;
+    uint256 public price1CumulativeLast;
+
+    bool private isEntered;
+
+    event Burn(
+        address indexed sender,
+        uint256 amount0,
+        uint256 amount1,
+        address to
+    );
+
+    event Mint(address indexed sender, uint256 amount0, uint256 amount1);
+    event Sync(uint256 reserve0, uint256 reserve1);
+    event Swap(
+        address indexed sender,
+        uint256 amount0Out,
+        uint256 amount1Out,
+        address indexed to
+    );
+
+    modifier nonReentrant() {
+        require(!isEntered);
+        isEntered = true;
+        _;
+        isEntered = false;
+    }
+
+    constructor() ERC20("ZuniswapV2", "ZUNI") {}
+
+    function _update(
+        uint256 balance0,
+        uint256 balance1,
+        uint112 reserve0_,
+        uint112 reserve1_
+    ) private {
+        if (balance0 > type(uint112).max || balance1 > type(uint112).max) {
+            revert BalanceOverflow();
+        }
+
+        //创建一个块，这个块内的溢出和下溢检查被禁用
+        //因为我们这里手动检查了
+        unchecked {
+            uint32 timeElapsed = uint32(block.timestamp) - blockTimestampLast;
+
+            if (timeElapsed > 0 && reserve0_ > 0 && reserve1_ > 0) {
+                price0CumulativeLast +=
+                    uint256(UQ112x112.encode(reserve1_).uqdiv(reserve0_)) *
+                    timeElapsed;
+                price1CumulativeLast +=
+                    uint256(UQ112x112.encode(reserve0_).uqdiv(reserve1_)) *
+                    timeElapsed;
+            }
+        }
+
+        reserve0 = uint112(balance0);
+        reserve1 = uint112(balance1);
+        blockTimestampLast = uint32(block.timestamp);
+
+        emit Sync(reserve0, reserve1);
+    }
 
     //底层函数：添加流动性，铸造LP token
-    function mint() public {
-        
+    function mint(to) public returns (uint256 liquidity) {
+        (reserve0, reserve1, ) = getReserves();
+        uint256 balance0 = IERC20(token0).balanceOf(address(this));
+        uint256 balance1 = IERC20(token1).balanceOf(address(this));
+
+        //计算添加的流动性的数量
+        uint256 amount0 = balance0 - reserve0;
+        uint256 amount1 = balance1 - reserve1;
+
+        uint256 liquidity;
+
+        //如果是初始流动性
+        if (totalSupply == 0) {
+            liquidity = Math.sqrt(amount0 * amount1) - MINIMUM_LIQUIDITY;
+            //锁定1000的流动性
+            _mint(address(0), liquidity);
+        } else {
+            liqudity = Math.min(
+                (amount0 * totalSupply) / reserve0,
+                (amount1 * totalSupply) / reserve1
+            );
+        }
+
+        if (liquidity <= 0) revert InsufficientLiquidityMinted();
+
+        _mint(to, liquidity);
+        _update(balance0, balance1, reserve0_, reserve1_);
+        emit Mint(to, amount0, amount1);
+    }
+
+    function getReserves() public view returns (uint112, uint112, uint32) {
+        return (reserve0, reserve1, blockTimestampLast);
     }
 }
